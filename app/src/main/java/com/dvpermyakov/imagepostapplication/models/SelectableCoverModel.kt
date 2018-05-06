23212ec186b0984bb395081ee413213ca3852164
dmitrypermyakov <dmitry.permyakov@fulldive.com>
2018-05-06 19:20:23 +0300

    fix writeToParcel() in SelectableCoverModel

--- app/src/main/java/com/dvpermyakov/imagepostapplication/models/SelectableCoverModel.kt
+++ app/src/main/java/com/dvpermyakov/imagepostapplication/models/SelectableCoverModel.kt
@@ -12,6 +12,7 @@ data class SelectableCoverModel(val cover: CoverModel, var selected: Boolean) : 
     constructor(parcel: Parcel) : this(parcel.readParcelable<CoverModel>(CoverModel::class.java.classLoader), parcel.readByte() != 0.toByte())
 
     override fun writeToParcel(dest: Parcel, flags: Int) = with(dest) {
+        writeParcelable(cover, flags)
         writeByte(if (selected) 1 else 0)
     }
 
@@ -19,5 +20,4 @@ data class SelectableCoverModel(val cover: CoverModel, var selected: Boolean) : 
         @JvmField
         val CREATOR = parcelableCreator(::SelectableCoverModel)
     }
-
 }
